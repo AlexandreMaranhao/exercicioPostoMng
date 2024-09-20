eac2b8393cb6a9c2af66286b5543ad360a6fb944
Maranhao <alexandre.maranhao@accenture.com>
2024-09-20 16:21:36 -0300

    chore: payment method was deleted

--- aluraAPI/src/main/java/com/aluraAPI/aluraAPI/domain/persistence/metodopagamento/MetodoPagamentoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.aluraAPI.aluraAPI.domain.persistence.metodopagamento;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MetodoPagamentoRepository extends JpaRepository<MetodoPagamento, Long> {
-}
--- aluraAPI/src/main/java/com/aluraAPI/aluraAPI/domain/persistence/metodopagamento/dto/DadosCadastroMetodoPagamento.java
+++
@@ -1,9 +0,0 @@
-package com.aluraAPI.aluraAPI.domain.persistence.metodopagamento.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record DadosCadastroMetodoPagamento(
-        @NotBlank
-        String metodo
-) {
-}

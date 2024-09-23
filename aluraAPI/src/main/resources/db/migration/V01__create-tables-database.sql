9f15099b8cec5809fa7f68e80cabfcc2e88297c3
Maranhao <alexandre.maranhao@accenture.com>
2024-09-23 11:25:31 -0300

    db changed

--- aluraAPI/src/main/resources/db/migration/V01__create-tables-database.sql
+++ aluraAPI/src/main/resources/db/migration/V01__create-tables-database.sql
@@ -11,7 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema posto
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `posto` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `posto`;
 USE `posto` ;
 
 -- -----------------------------------------------------
